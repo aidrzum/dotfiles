0e09f8c88cf2c080dc5a9418113a5143e0107442
aidr <aidrzum@gmail.com>
2025-08-25 23:45:08 +0545

    updated son @@ ;)

--- 34-2configurations.nix
+++ 34-2configurations.nix
@@ -1,53 +1,24 @@
 { config, pkgs, ... }:
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
     ./hardware-configuration.nix
     ];
-# Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "nix-box"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-# Enable networking
     networking.networkmanager.enable = true;
-# Set your time zone.
   time.timeZone = "Asia/Kathmandu";
-# Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-# Enable the X11 windowing system.
   services.xserver.enable = true;
-# Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.power-profiles-daemon.enable = true;
   services.gvfs.enable = true;
-#services.xserver.desktopManager.gnome.enable = true;
-#services.xserver.desktopManager.gnome-keyring.enable = true;
-# Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-#Thermald 
   services.thermald.enable = true;
-# Enable the autocpufreq
-#  services.auto-cpufreq.enable = true;
-#  services.auto-cpufreq.settings = {
-#    battery = {
-#      governor = "powersave";
-#      turbo = "never";
-#    };
-#    charger = {
-#      governor = "performance";
-#      turbo = "auto";
-#    };
-#  };
-# Enable CUPS to print documents.
-#services.printing.enable = true;
-# Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -55,11 +26,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-# If you want to use JACK applications, uncomment this
     jack.enable = true;
-# use the example session manager (no others are packaged yet so this is enabled by default,
-# no need to redefine it in your config for now)
-#media-session.enable = true;
   };
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
@@ -67,25 +34,19 @@
     vaapiIntel
       intel-media-driver
   ];
-# Enable touchpad support (enabled default in most desktopManager).
-# services.xserver.libinput.enable = true;
-# Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.aidr.shell = pkgs.zsh;
   users.users.aidr = {
     isNormalUser = true;
     description = "aidr";
     extraGroups = [ "networkmanager" "wheel" "audio" "video" "input" "libvirtd" "storage" ];
     packages = with pkgs; [
-#  thunderbird
     ];
   };
-# Install firefox.
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
-
     shellAliases = {
       la = "ls -al";
       edit = "sudo -e";
@@ -101,8 +62,6 @@
       yy = "yazi";
       neof="neo -c ~/.config/fastfetch/config-compact.jsonc";
       UP = "python3 -m http.server 8181";
-
-
     };
     histSize = 10000;
     histFile = "$HOME/.zsh_history";
@@ -120,11 +79,6 @@
       theme = "random";
     };
   };
-
-#   home.sessionPath = [
-#       "~/.config/emacs/bin"
-#   ];
-
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
@@ -383,22 +337,19 @@
     };
   };
 
-  programs.waybar.enable = true;
-
-
-
-  programs.firefox.enable = true;
-  programs.virt-manager.enable = true;
+  programs = {
+    waybar.enable = true;
+    firefox.enable = true;
+    virt-manager.enable = true;
+  };
   users.groups.libvirtd.members = ["aidr"];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-#programs.sway.enable = true;
-# Allow unfree packages
+  virtualisation = {
+    libvirtd.enable = true;
+    spiceUSBRedirection.enable = true;
+  };
   nixpkgs.config.allowUnfree = true;
-# List packages installed in system profile. To search, run:
-# $ nix search wget
   environment.systemPackages = with pkgs; [
-    adwaita-icon-theme
+      adwaita-icon-theme
       adwaita-icon-theme
       adwaita-qt
       adwaita-qt6
@@ -514,14 +465,12 @@
       zoxide
       zsh-autosuggestions
       zsh-syntax-highlighting
-      ];
+  ];
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
       nerd-fonts.fantasque-sans-mono
       nerd-fonts.agave
   ];
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
 # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
@@ -541,8 +490,6 @@
     6667
     445
   ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
   networking.firewall.enable = true;
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
